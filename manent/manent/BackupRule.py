beb70e1a846586d0bfee2a95f69d4bbd2039908d
genady <genady>
2006-12-16 13:59:15 +0000

    Initial API prototype

---
+++ manent/manent/BackupRule.py
@@ -0,0 +1,16 @@
+class BackupRule:
+	
+	def getFiles(self):
+		pass
+	
+	def addIncludedTree(self, directory):
+		pass
+	
+	def addExcludedSubTree(self, directory):
+		pass
+	
+	def addExcludedFilePattern(self, directory):
+		pass
+	
+	def setBackupFrequecy(self, months, days, hours, minutes):
+		pass
