fafc0b04c24ce764e1bc31f3c9c97780c49b5974
genady <genady>
2006-12-17 00:41:28 +0000

    simple implementation for the ui

--- manent/manent/BackupRule.py
+++ manent/manent/BackupRule.py
@@ -1,16 +1,35 @@
+import os
+
 class BackupRule:
+	def __init__(self):
+		self.included = []
+		self.excluded = []
+		self.excludedFilePatterns = []
 	
 	def getFiles(self):
 		pass
 	
 	def addIncludedTree(self, directory):
-		pass
+		self.included.append(directory)
 	
 	def addExcludedSubTree(self, directory):
-		pass
+		self.excluded.append(directory)
 	
-	def addExcludedFilePattern(self, directory):
-		pass
+	def addExcludedFilePattern(self, pattern):
+		self.excludedFilePatterns.append(pattern)
+	
+	def isIncluded(self, path):
+		for i in self.included:
+			if path.startswith(i):
+				return True
+		return False
+		# check that it's not excluded.
 	
 	def setBackupFrequecy(self, months, days, hours, minutes):
 		pass
+
+# test
+#rule= BackupRule()
+#rule.addIncludedTree("c:\\windows\\system32")
+#rule.addIncludedTree("c:\\windows\\system")
+#print rule.isIncluded("c:\\windows\\system\\a.txt")
