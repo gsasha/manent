d0c65ec51435cde06188b19c9e14762b2fdbc145
gsasha <gsasha>
2006-12-21 16:27:12 +0000

    Ok, we're python 2.5-only now. moving back to hashlib and to sha256

--- manent/manent/utils/Digest.py
+++ manent/manent/utils/Digest.py
@@ -1,22 +1,24 @@
 import md5
 import struct
+import hashlib
 
 #
 # Hashing parameters
 #
 def dataDigest(data):
-	#return hashlib.sha256(data).digest()
-	h = md5.new(struct.pack("B",len(data)%256))
+	h = hashlib.sha256(data)
+	#h = md5.new()
+	h.update(struct.pack("B",len(data)%256))
 	h.update(data)
 	return h.digest()
 
 def dataDigestSize():
-	#return 32
-	return 16
+	return 32
+	#return 16
 
 def headerDigest(data):
-	#return hashlib.md5(data).digest()
-	return md5.md5(data).digest()
+	return hashlib.md5(data).digest()
+	#return md5.md5(data).digest()
 
 def headerDigestSize():
 	return 16
