f7f5aa0a5d00bd1c88fe271c3fd7e71826331cbd
Alex (Sasha) Gontmakher <gsasha@gsasha.(none)>
2007-12-18 10:07:50 +0100

    Added plan for testing of Repository

--- manent/testsuite/TestRepository.py
+++ manent/testsuite/TestRepository.py
@@ -4,5 +4,18 @@ import tempfile
 import unittest
 
 class TestRepository(unittest.TestCase):
-	def test1(self):
+	def test_add_new_storage(self):
+		"""Test that adding a new storage creates it correctly"""
+		self.fail()
+	def test_add_existing_storage(self):
+		"""Test that adding an existing storage imports and maps the storage correctly"""
+		self.fail()
+	def test_rescan_storage(self):
+		"""Test that new sequences appearing from outside are discovered"""
+		self.fail()
+	def test_add_block(self):
+		"""Test that if blocks are added, they are available for loading back"""
+		self.fail()
+	def test_container(self):
+		"""Test that containers are created when necessary"""
 		self.fail()
