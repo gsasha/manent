b2846092efea13a096d6611219afff7772bfb707
Alex (Sasha) Gontmakher <gsasha@gmail.com>
2008-05-11 14:41:23 +0200

    Fixed TestIncrement

--- manent/testsuite/TestIncrement.py
+++ manent/testsuite/TestIncrement.py
@@ -3,7 +3,14 @@
 #    License: see LICENSE.txt
 #
 
+import logging
+import os
 import unittest
+
+# Point to the code location so that it is found when unit tests
+# are executed. We assume that sys.path[0] is the path to the module
+# itself. This allows the test to be executed directly by testoob.
+sys.path.append(os.path.join(sys.path[0], ".."))
 
 import manent.Database as Database
 import manent.Increment as Increment
