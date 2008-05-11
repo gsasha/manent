e486f6f2fba61ab3abef1164317cdc32d0b3c24d
Alex (Sasha) Gontmakher <gsasha@gmail.com>
2008-05-11 14:37:01 +0200

    Fixed TestExclusionProcessor

--- manent/testsuite/TestExclusionProcessor.py
+++ manent/testsuite/TestExclusionProcessor.py
@@ -3,8 +3,15 @@
 #    License: see LICENSE.txt
 #
 
+import logging
+import os
+import os.path
 import unittest
-import os, os.path
+
+# Point to the code location so that it is found when unit tests
+# are executed. We assume that sys.path[0] is the path to the module
+# itself. This allows the test to be executed directly by testoob.
+sys.path.append(os.path.join(sys.path[0], ".."))
 
 import manent.ExclusionProcessor as EP
 import UtilFilesystemCreator as FSC
