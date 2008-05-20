d2d0b8cb4bd682dc712383394ff3f1a97bfc84f5
Alex (Sasha) Gontmakher <gsasha@gmail.com>
2008-05-21 00:35:03 +0200

    retab

--- src/Manent.py
+++ src/Manent.py
@@ -24,80 +24,80 @@ config.load()
 #  Print help message
 #
 if (len(sys.argv)==1) or (sys.argv[1]=="help"):
-	print "Possible commands: create, configure, backup, info, restore"
-	print "Available backups:"
-	for label in config.list_backups():
-		print "  ", label
-	sys.exit(0)
+  print "Possible commands: create, configure, backup, info, restore"
+  print "Available backups:"
+  for label in config.list_backups():
+    print "  ", label
+  sys.exit(0)
 #
 #  Create a new backup set
 #
 elif sys.argv[1] == "create":
-	label = sys.argv[2]
+  label = sys.argv[2]
 
-	if config.has_backup(label):
-		print "Backup config", label, "already exists"
-		sys.exit(0)
-	backup = config.create_backup(label)
+  if config.has_backup(label):
+    print "Backup config", label, "already exists"
+    sys.exit(0)
+  backup = config.create_backup(label)
 
-	config.save()
-	config.close()
+  config.save()
+  config.close()
 #
 # Reconstruct the backup set from medias
 #
 elif sys.argv[1] == "configure":
-	label = sys.argv[2]
-	if not config.has_backup(label):
-		print "Backup config", label, "does not exist"
-		sys.exit(0)
-	backup = config.load_backup(label)
-	backup.configure(sys.argv[3:])
+  label = sys.argv[2]
+  if not config.has_backup(label):
+    print "Backup config", label, "does not exist"
+    sys.exit(0)
+  backup = config.load_backup(label)
+  backup.configure(sys.argv[3:])
 
-	config.save()
-	config.close()
+  config.save()
+  config.close()
 #
 #  Do the backup
 #
 elif sys.argv[1] == "backup":
-	label = sys.argv[2]
+  label = sys.argv[2]
 
-	backup = config.load_backup(label)
+  backup = config.load_backup(label)
   backup.scan(sys.argv[3:])
-	
-	config.save()
-	config.close()
+  
+  config.save()
+  config.close()
 #
 # Do the restoration
 #
 elif sys.argv[1] == "restore":
-	label = sys.argv[2]
+  label = sys.argv[2]
 
-	backup = config.load_backup(label)
-	backup.restore(sys.argv[3:])
-	config.close()
+  backup = config.load_backup(label)
+  backup.restore(sys.argv[3:])
+  config.close()
 
 elif sys.argv[1] == "test":
-	label = sys.argv[2]
+  label = sys.argv[2]
 
-	backup = config.load_backup(label)
-	backup.test(sys.argv[3:])
-	config.close()
+  backup = config.load_backup(label)
+  backup.test(sys.argv[3:])
+  config.close()
 
 elif sys.argv[1] == "remove":
-	label = sys.argv[2]
-	config.remove_backup(label)
-	config.save()
-	config.close()
+  label = sys.argv[2]
+  config.remove_backup(label)
+  config.save()
+  config.close()
 
 elif sys.argv[1] == "info":
-	label = sys.argv[2]
-	backup = config.load_backup(label)
-	backup.info(sys.argv[3:])
-	#config.save()
-	config.close()
+  label = sys.argv[2]
+  backup = config.load_backup(label)
+  backup.info(sys.argv[3:])
+  #config.save()
+  config.close()
 
 else:
-	print "Unknown command", sys.argv[1]
+  print "Unknown command", sys.argv[1]
 
 sys.exit(0)
 
